9212e91192272420cb343d5819718cf6ebcf9862
BuildTools <akulehtonen@hotmail.com>
2018-04-18 03:49:07 +0300

    Update content

---
+++ GamePlanet/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePlanet
+{
+    class Product
+    {
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+            
+        }
+    }
+}
---
+++ GamePlanet/Shop.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GamePlanet
+{
+    /// <summary>
+    /// Interaction logic for Shop.xaml
+    /// </summary>
+    public partial class Shop : Window
+    {
+        public Shop()
+        {
+            InitializeComponent();
+            FillData();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void FillData()
+        {
+            try
+            {
+                SqlConnect connection = new SqlConnect();
+                dgProducts.ItemsSource = connection.GetProducts();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+
+}
