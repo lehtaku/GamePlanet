a9c058b3667b6c61061f0d98fadafff3961c2a36
Leevi Kopakkala <leevi.kopakkala@hotmail.com>
2018-04-19 00:01:10 +0300

    Fix Window location

--- GamePlanet/MainWindow.xaml.cs
+++ GamePlanet/MainWindow.xaml.cs
@@ -30,6 +30,15 @@ namespace GamePlanet
             if (e.ChangedButton == MouseButton.Left)
                 this.DragMove();
         }
+        private void CenterWindowOnScreen()
+        {
+            double screenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+            double screenHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+            double windowWidth = this.Width;
+            double windowHeight = this.Height;
+            this.Left = (screenWidth / 2) - (windowWidth / 2);
+            this.Top = (screenHeight / 2) - (windowHeight / 2);
+        }
     }
 
 
