9bf5c865f8cb8fa6b89ed03a0a37d0da423d9038
Leevi Kopakkala <leevi.kopakkala@hotmail.com>
2018-04-21 14:25:10 +0300

    Add create user

--- GamePlanet/Createuser.xaml.cs
+++ GamePlanet/Createuser.xaml.cs
@@ -10,23 +10,21 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace GamePlanet
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Createuser.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Createuser : Window
     {
-        public MainWindow()
+        public Createuser()
         {
             InitializeComponent();
         }
 
-
-            private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
                 this.DragMove();
@@ -41,28 +39,19 @@ namespace GamePlanet
             this.Top = (screenHeight / 2) - (windowHeight / 2);
         }
 
-        public void btnLogin_Click(object sender, RoutedEventArgs e)
+        public void btnCancel_Click(object sender, RoutedEventArgs e)
         {
 
-            Login user = new Login();
-            user.Username = txbUsername.Text;
-            user.Password = pwdPassword.Password;
+            MainWindow mainwindow = new MainWindow();
+            mainwindow.Show();
+            this.Close();
 
-            string name = user.Username;
-            string pwd = user.Password;
+        }
 
-            int login = DBLogin.MySQLLogin(name, pwd);
-            if (login == 1)
-            {
-                Shop shop = new Shop();
-                shop.Show();
-                this.Close();
-            }
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
 
         }
     }
-
-
-
-
 }
+
--- GamePlanet/DBLogin.cs
+++ GamePlanet/DBLogin.cs
@@ -65,6 +65,61 @@ namespace GamePlanet
 
         }
 
+        public static int MySQLCreate(string username, string password)
+        {
+
+            try
+            {
+
+                string connStr = GetConnectionString();
+                //string sql = string.Format("SELECT UserName FROM User HAVING UserName={0}", user);
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(@"INSERT INTO User (FirstName, LastName, UserName, Email, Password, CreatedAt) VALUES", conn);
+                    cmd.Parameters.AddWithValue("@uname", username);
+                    cmd.Parameters.AddWithValue("@pass", password);
+
+                    int result = Convert.ToInt32(cmd.ExecuteScalar());
+                    if (result > 0)
+                    {
+                        MessageBox.Show("Login Success");
+                        return result;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Username or Password is incorrect");
+                        return result;
+                    }
+                }
+
+
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return 0;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return 0;
+            }
+
+
+        }
+
         private static string GetConnectionString()
         {
 
@@ -72,5 +127,7 @@ namespace GamePlanet
             return string.Format("Data source=mysql.labranet.jamk.fi;Initial Catalog=K8292_2;user=K8292;password={0}", password);
         }
     }
+
 }
+
 
