1d0186c51bd1a1cddeb0f068b5b69d4cb303a90a
Leevi Kopakkala <leevi.kopakkala@hotmail.com>
2018-04-24 16:06:17 +0300

    Hotfix purchase

--- GamePlanet/Purchase.xaml.cs
+++ GamePlanet/Purchase.xaml.cs
@@ -23,5 +23,11 @@ namespace GamePlanet
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
     }
 }
